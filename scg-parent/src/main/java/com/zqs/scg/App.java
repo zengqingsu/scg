2f40728582a47265745708cd89324071c01c0ae9
zengqingsu <zeng_qingsu@163.com>
2017-11-10 18:32:03 +0800

    commit

---
+++ scg-parent/src/main/java/com/zqs/scg/App.java
@@ -0,0 +1,13 @@
+package com.zqs.scg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
